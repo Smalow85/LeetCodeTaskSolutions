1f3671f28a7b4622c4ac7ea008697d535913fef7
smalow85 <smalow@mail.ru>
2024-04-29 23:03:45 +0300

    Repo with Leet Code Task Solutions

--- src/test/java/org/example/easy/SearchInsertPositionTest.java
+++ src/test/java/org/example/easy/SearchInsertPositionTest.java
@@ -1,14 +1,12 @@
 package org.example.easy;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Map;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SearchInsertPositionTest {
 
