b858ff426122cf923d3295451cdc8485aa3c11c4
smalow85 <smalow@mail.ru>
2024-04-29 23:03:30 +0300

    Repo with Leet Code Task Solutions

---
+++ src/test/java/org/example/easy/SearchInsertPositionTest.java
@@ -0,0 +1,33 @@
+package org.example.easy;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SearchInsertPositionTest {
+
+    @ParameterizedTest
+    @MethodSource("hashMapProvider")
+    public void testSearchInsertPosition(Map<Integer, Integer> arguments) {
+        int[] nums = {1,3,5,6};
+        arguments.entrySet().forEach(e -> {
+            int res = SearchInsertPosition.searchInsert(nums, e.getKey());
+            assertEquals(e.getValue(), res);
+        });
+    }
+
+    static Stream<Map<Integer, Integer>> hashMapProvider() {
+        return Stream.of(
+                Map.of(0, 0),
+                Map.of(1, 0),
+                Map.of(2, 1),
+                Map.of(8, 4)
+        );
+    }
+}
